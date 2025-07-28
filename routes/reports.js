97639f7299585d8ad9e13ff569afa25cc2a2c021
Darwing Diaz <darwingadiaz@gmail.com>
2025-07-27 21:07:49 -0400

    new campos

--- routes/reports.js
+++ routes/reports.js
@@ -22,13 +22,10 @@ router.get('/', async (req, res) => {
     res.status(500).json({ error: 'Error al obtener los reportes' });
   }
 });
-
-
 router.get('/totales', async (req, res) => {
   try {
     const { zona, desde, hasta } = req.query;
 
-    console.log(zona, desde, hasta);
 
     const match = {};
 
@@ -69,6 +66,10 @@ router.get('/totales', async (req, res) => {
         convertidos: { $sum: "$convertidos" },
         discipulados: { $sum: "$discipulados" },
         ofrendas: { $sum: "$ofrendas" },
+        kids : { $sum: "$kids" },
+        house: { $sum: "$house" },
+        trate: { $sum: "$trate" },
+        reconciliados: { $sum: "$reconciliados" },
       },
     });
 
@@ -82,6 +83,10 @@ router.get('/totales', async (req, res) => {
         convertidos: 0,
         discipulados: 0,
         ofrendas: 0,
+        kids: 0,
+        house: 0,
+        trate: 0,
+        reconciliados: 0,
       });
     }
 
@@ -94,6 +99,52 @@ router.get('/totales', async (req, res) => {
     });
   }
 });
+router.get('/:id', async (req, res) => {
+  try {
+    const report = await Report.findById(req.params.id);
+
+    if (!report) {
+      return res.status(404).json({ error: 'Reporte no encontrado' });
+    }
+
+    res.json(report);
+  } catch (err) {
+    console.error('Error al obtener el reporte:', err);
+    res.status(500).json({ error: 'Error al obtener el reporte' });
+  }
+});
+router.delete('/:id', async (req, res) => {
+  try {
+    const deleted = await Report.findByIdAndDelete(req.params.id);
+    if (!deleted) {
+      return res.status(404).json({ message: 'Reporte no encontrado' });
+    }
+    res.json({ message: 'Reporte eliminado correctamente' });
+  } catch (error) {
+    console.error('Error al eliminar reporte:', error);
+    res.status(500).json({ error: 'Error del servidor' });
+  }
+});
+router.put('/:id', async (req, res) => {
+  const { evangelizados, sanidades,  convertidos, discipulados, ofrendas, house, trate, kids, reconciliados } = req.body;
+
+  try {
+    const updatedReport = await Report.findByIdAndUpdate(
+      req.params.id,
+      { evangelizados, sanidades, convertidos, discipulados, ofrendas, house, trate, kids, reconciliados },
+      { new: true }
+    );
+
+    if (!updatedReport) {
+      return res.status(404).json({ error: 'Reporte no encontrado' });
+    }
+
+    res.json(updatedReport);
+  } catch (err) {
+    res.status(500).json({ error: 'Error al actualizar el reporte' });
+  }
+});
+
 
 
 module.exports = router;
